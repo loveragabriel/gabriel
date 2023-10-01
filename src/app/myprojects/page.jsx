fc4fe95468f6b72f26665f7c7e4ddf847d8c9444
Gabriel <loveragabriel20@gmail.com>
2023-10-01 19:04:57 -0300

    creo feature project

--- src/app/myprojects/page.jsx
+++ src/app/myprojects/page.jsx
@@ -1,9 +1,10 @@
+import styles from './projects.module.css';
 import Title from '@/components/pageTitle/Title'
 export default function Projects(){
     return(
         <>
         <Title Title='Projects'/>
-        <p>This is projects</p>
+        <p className={styles.main_text}>Here are some of the projects that I had worked. </p>
         </>
     )
 }
