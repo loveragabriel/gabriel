5af941a9c2402041a399625daff5065cd95df72f
Gabriel <loveragabriel20@gmail.com>
2023-10-01 00:11:57 -0300

    agrego animation a hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,14 @@ import styles from './page.module.css'
 
 export default function Home() {
   return (
-    <main className={styles.main} >
+    <main className={styles.main} > 
       <h1 className={styles.main_text}>
         <span className={styles.text_name}>Gabriel Lovera</span>
-         <span className={styles.text_title}>Desarrollador React</span>
+        <span className={styles.text_title}>Desarrollador React</span>
       </h1>
+      <section>
+
+      </section>
       <section>
         Diseño y desarrollo de interfaces de usuario. Aprendo, programo e itero.
       </section>
