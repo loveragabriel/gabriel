e62e1f41ee5375653ad70c0163a38d0fec23ccce
Gabriel <loveragabriel20@gmail.com>
2023-10-01 02:27:29 -0300

    agrego hover al call to action

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import HeroButton from '@/components/heroButton/Hero_Button'
 
 export default function Home() {
   return (
     <main className={styles.main} > 
       <h1 className={styles.main_text}>
         <span className={styles.text_name}>Gabriel Lovera</span>
-        <span className={styles.text_title}>Desarrollador React</span>
+        <span className={styles.text_title}>Desarrollador React
+        <span className={styles.button_call_to_action}>
+        <  HeroButton/>
+        </span>
+        </span>
       </h1>
       <section>
 
---
+++ src/components/heroButton/Hero_Button.jsx
@@ -0,0 +1,6 @@
+import styles from './hero_button.module.css'
+export default function Hero_Button(){
+    return(
+        <button className={styles.button}>Contact me</button>
+    )
+}
