cc2be4aaaad13e6b1d6deb3a861f9734eefac888
Gabriel <loveragabriel20@gmail.com>
2023-10-03 17:30:36 -0300

    pull done

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,4 +1,4 @@
-mport styles from './contact.module.css'
+import styles from './contact.module.css'
 import Title from '@/components/pageTitle/Title'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLinkedin } from "@fortawesome/free-solid-svg-icons";
@@ -8,7 +8,6 @@ export default function Contact() {
         <>
             <Title Title='Contact' />
             <p clasessName={styles.contact_text}>Let's connect if you need a website for your business, a partner for your project or a talent for complete a teamwork. I would like to talk about new challenges.
-                Let's connect if you need a website for your business, a partner for your project or a talent for complete a teamwork. I would like to talk about new challenges.
             </p>
             <section clasessName={styles.nextworks}>
                 <FontAwesomeIcon icon={faLinkedin} className={styles.icons} />
