9c68e948c75202ec05834ebaf69dc65e67b42086
Gabriel Lovera <87627423+loveragabriel@users.noreply.github.com>
2023-10-03 12:53:09 -0300

    Update Hero_Button.jsx
    Agrego enlace a la pagina Contact

--- src/components/heroButton/Hero_Button.jsx
+++ src/components/heroButton/Hero_Button.jsx
@@ -1,6 +1,9 @@
 import styles from './hero_button.module.css'
+import Link from 'next/link'
+
 export default function Hero_Button(){
     return(
+ <Link href='/contact'>
         <button className={styles.button}>Contact me</button>
-    )
+    </Link>    )
 }
