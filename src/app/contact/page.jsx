03c5207376930e2741589e17a1af69d615fe5529
Gabriel <loveragabriel20@gmail.com>
2023-10-04 01:43:04 -0300

    contact page fix

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,17 +1,21 @@
-import styles from './contact.module.css'
+import styles from './contact.module.css';
 import Title from '@/components/pageTitle/Title'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLinkedin } from "@fortawesome/free-solid-svg-icons";
-
+import {
+    faLinkedin,
+    faGitAlt,
+  } from "@fortawesome/free-brands-svg-icons";
 export default function Contact() {
     return (
         <>
             <Title Title='Contact' />
-            <p clasessName={styles.contact_text}>Let's connect if you need a website for your business, a partner for your project or a talent for complete a teamwork. I would like to talk about new challenges.
+            <p clasessName={styles.contact_text}>Let&lsquo;s connect if you need a website for your business, a partner for your project or a talent for complete a teamwork. I would like to talk about new challenges.
             </p>
             <section clasessName={styles.nextworks}>
-                <FontAwesomeIcon icon={faLinkedin} className={styles.icons} />
-            </section>
+            <FontAwesomeIcon icon={faLinkedin} className={styles.icons} />
+ 
+        <FontAwesomeIcon icon={faGitAlt} className={styles.icons} />
+           </section>
         </>
     )
 }
